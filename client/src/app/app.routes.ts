f3add6ccd8b284af4b9dc87a362c36fc8e00b8ba
Cxderzz <sorenjameskelly@gmail.com>
2025-09-23 15:18:02 +1000

    feat: add page routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/quote/start',
+    pathMatch: 'full',
+  },
+  {
+    path: 'quote/start',
+    loadComponent: () =>
+      import('./modules/quote-flow/quote-start/quote-start').then((m) => m.QuoteStartComponent),
+  },
+];
